6fc9e525ce3902800a541982e814b2b56820d9b4
Rerif <zahar_9898@mail.ru>
2020-09-14 16:52:58 +0600

    Основной алгоритм закончен. Необходима реализация температуры и энергии

--- Kursovoy_bak_1/KP_Python_v2.py
+++ Kursovoy_bak_1/KP_Python_v2.py
@@ -3,7 +3,7 @@ from method_k_ya import *
 
 
 # кол-во ячеек 
-cell_count = 10
+cell_count = 100
 # кол-во временных отрезков
 n = 100
 P0 = 100000
@@ -12,6 +12,7 @@ P01 = 98000
 P02 = 50000
 T01 = 20
 T02 = 80
+Rg = 8.31
 w1 = 70
 w2 = 100
 d = 0.3
@@ -19,8 +20,8 @@ l = 0.2
 k = 0.001
 D1 = 0.6
 D2 = 0.9 
-rho = 1000
-te = 0.5
+rhoi = 1000
+te = 0.1
 # площадь канала
 A = math.pi * d * d / 4
 # Coefficients of local resistance calculation.
@@ -47,55 +48,49 @@ M = 0
 dM = 0
 dx = l/cell_count
 
-pole = [Yacheyka(i, 0, (P01+P02)/2, rho) for i in range(cell_count)]
+pole = [Yacheyka(i, 0, (P01+P02)/2, rhoi, (T01+T02)/2) for i in range(cell_count)]
 
 # начальные условия
 ei_n = 0
 # Main cycle.                            
 while t<=te:                                 
 	P1 = P01*math.cos(w1*t)+P0
-	#T1 = T01*math.cos(w1*t)+T0
+	T1 = T01*math.cos(w1*t)+T0
 	P2 = P02*math.cos(w2*t)+P0
-	#T2 = T02*math.cos(w2*t)+T0
+	T2 = T02*math.cos(w2*t)+T0
 	pole[0].change_pressure(P1)
 	pole[cell_count-1].change_pressure(P2)
 
-
-
-
-	
-
-
-
+	# первый этап
 	for j in range(cell_count):
 		# первый этап
 		yacheyka = pole[j]
 		# параметры газа относящиеся к границам ячеек
 		# Pi_plus_05_n = (Pi_n+Pi_plus_1_n)/2
-		Pi_n = pole[j].P
-		Wi_n = pole[j].W
+		Pi_n = yacheyka.Pi_n
+		Wi_n = yacheyka.Wi_n
 		# граничные условия 1
 		if j == 0:
 			Pi_minus_1_n = Pi_n
 			Wi_minus_1_n = Wi_n
-			Pi_plus_1_n = pole[j+1].P
-			Wi_plus_1_n = pole[j+1].W
+			Pi_plus_1_n = pole[j+1].Pi_n
+			Wi_plus_1_n = pole[j+1].Wi_n
 			prev_yacheyka = pole[j]
 
 		elif j == cell_count-1:
 			Pi_plus_1_n = Pi_n
 			Wi_plus_1_n = Wi_n
-			Pi_minus_1_n = pole[j-1].P
-			Wi_minus_1_n = pole[j-1].W
+			Pi_minus_1_n = pole[j-1].Pi_n
+			Wi_minus_1_n = pole[j-1].Wi_n
 			next_yacheyka = pole[j]
 
 		else:
 			prev_yacheyka = pole[j-1]
 			next_yacheyka = pole[j+1]
-			Pi_plus_1_n = pole[j+1].P 
-			Pi_minus_1_n = pole[j-1].P
-			Wi_plus_1_n = pole[j+1].W
-			Wi_minus_1_n = pole[j-1].W
+			Pi_plus_1_n = pole[j+1].Pi_n
+			Pi_minus_1_n = pole[j-1].Pi_n
+			Wi_plus_1_n = pole[j+1].Wi_n
+			Wi_minus_1_n = pole[j-1].Wi_n
 
 		Pi_minus_05_n = (Pi_n+Pi_minus_1_n)/2
 		Pi_plus_05_n = (Pi_n+Pi_plus_1_n)/2
@@ -105,14 +100,19 @@ while t<=te:
 
 		# промежуточные значения (значения на первом этапе) скорости и энергии газа
 		# на временном слое n+1
-		Wi_n_plus_1 = Wi_n - ((Pi_plus_05_n-Pi_minus_05_n)/dx)*(dt/rho)
-		ei_n_plus_1 = ei_n - ((Pi_plus_05_n*Wi_plus_05_n-Pi_minus_05_n*Wi_minus_05_n)/dx)*(dt/rho)
+		Wi_n_plus_1_vector = Wi_n - ((Pi_plus_05_n-Pi_minus_05_n)/dx)*(dt/rhoi)
+		#ei_n_plus_1 = ei_n - ((Pi_plus_05_n*Wi_plus_05_n-Pi_minus_05_n*Wi_minus_05_n)/dx)*(dt/rhoi)
 
-		yacheyka.change_speed(Wi_plus_1_n)
-		yacheyka.change_energy(ei_n_plus_1)
-		
+		yacheyka.Wi_n_vector = Wi_n_plus_1_vector
+		yacheyka.Wi_plus_05_n = Wi_plus_05_n
+		yacheyka.Wi_minus_05_n = Wi_minus_05_n
+		yacheyka.Pi_plus_05_n = Pi_plus_05_n
+		yacheyka.Pi_minus_05_n = Pi_minus_05_n
+		yacheyka.Ti_n = (T1+T2)/2
+		#yacheyka.change_energy(ei_n_plus_1)
 
 
+	# второй этап
 	for j in range(cell_count):
 		yacheyka = pole[j]
 		if j == 0:
@@ -125,40 +125,76 @@ while t<=te:
 			prev_yacheyka = pole[j-1]
 			next_yacheyka = pole[j+1]
 
-		Wi_n_plus_1 = yacheyka.W
-		Wi_plus_1_n_plus_1 = next_yacheyka.W
-		Wi_minus_1_n_plus_1 = prev_yacheyka.W
-		# второй этап
+		Wi_n_plus_1_vector = yacheyka.Wi_n_vector
+		Wi_plus_1_n_plus_1_vector = next_yacheyka.Wi_n_vector
+		Wi_minus_1_n_plus_1_vector = prev_yacheyka.Wi_n_vector
+		
 		# потоки массы газа за dt через границы ячейки
 
 		# масса газа прошедшая через правую границу i-ячейки при площади A    
-		if Wi_n_plus_1+Wi_plus_1_n_plus_1 > 0:
-			yacheyka.change_dM_plus((rho*(Wi_n_plus_1+Wi_plus_1_n_plus_1)*A*dt)/2)
+		if Wi_n_plus_1_vector+Wi_plus_1_n_plus_1_vector > 0:
+			yacheyka.change_dMi_plus_05_n((yacheyka.rhoi_n*(Wi_n_plus_1_vector+Wi_plus_1_n_plus_1_vector)*A*dt)/2)
 		else:
-			yacheyka.change_dM_plus((rho*(Wi_n_plus_1+Wi_plus_1_n_plus_1)*A*dt)/2)
+			yacheyka.change_dMi_plus_05_n((yacheyka.rhoi_n*(Wi_n_plus_1_vector+Wi_plus_1_n_plus_1_vector)*A*dt)/2)
 
 		# масса газа проходящая через левую границу i-й ячейки
-		if Wi_n_plus_1+Wi_minus_1_n_plus_1 > 0:
-			yacheyka.change_dM_minus((rho*(Wi_n_plus_1+Wi_plus_1_n_plus_1)*A*dt)/2)
+		if Wi_n_plus_1_vector+Wi_minus_1_n_plus_1_vector > 0:
+			yacheyka.change_dMi_minus_05_n((yacheyka.rhoi_n*(Wi_n_plus_1_vector+Wi_plus_1_n_plus_1_vector)*A*dt)/2)
 		else:
-			yacheyka.change_dM_minus((rho*(Wi_n_plus_1+Wi_minus_1_n_plus_1)*A*dt)/2)
+			yacheyka.change_dMi_minus_05_n((yacheyka.rhoi_n*(Wi_n_plus_1_vector+Wi_minus_1_n_plus_1_vector)*A*dt)/2)
 		    
-	
+		if j == 0:
+			if yacheyka.dMi_minus_05_n < 0:
+				M1 += abs(yacheyka.dMi_minus_05_n)
+			if t<dt:
+				print(yacheyka.dMi_minus_05_n)
+		elif j == cell_count-1:
+			if yacheyka.dMi_plus_05_n > 0:
+				M2 += yacheyka.dMi_plus_05_n
 
 	# третий этап
-	for i in range(cell_count-1):	    
-		rhoi_n_plus_1 = rhoi + (dMi_minus_05 - dMi_plus_05)/(A*dx)
-		Wi_n_plus_1 = rhoi_n * Wi_n / rhoi_n_plus_1 + (abs(dMi_minus_05_n)*Wi_minus_05_n_minus_1 - abs(dMi_plus_05_n) * Wi_plus_05_n_plus_1)/(rhoi_n_plus_1*A*dx)
-		ei_n_plus_1 = rhoi_n * ei_n / rhoi_n_plus_1 + (dMi_minus_05_n * ei_minus_05_n_plus_1 - dMi_plus_05_n * ei_plus_05_n_plus_1)/(rhoi_n_plus_1 * A * dx)
-
-		if Wi_n_plus_1+Wi_plus_1_n_plus_1 > 0:
-			Wi_plus_05_n_plus_1 = Wi_n_plus_1
-			ei_plus_1_n_plus_1 = ei_n_plus_1
+	for j in range(cell_count):
+		yacheyka = pole[j]
+		if j == 0:
+			prev_yacheyka = pole[j]
+		elif j == cell_count - 1:
+			next_yacheyka = pole[j]
 		else:
-			Wi_plus_05_n_plus_1 = Wi_plus_1_n_plus_1
-			ei_plus_1_n_plus_1 = ei_plus_1_n_plus_1
+			prev_yacheyka = pole[j-1]
+			next_yacheyka = pole[j+1]
+		
+		Wi_n_plus_1_vector = yacheyka.Wi_n_vector
+		Wi_plus_1_n_plus_1_vector = next_yacheyka.Wi_n_vector
+		
+		if Wi_n_plus_1_vector+Wi_plus_1_n_plus_1_vector > 0:
+			Wi_plus_05_n_plus_1_vector = Wi_n_plus_1_vector
+			#ei_plus_1_n_plus_1_vector = ei_n_plus_1_vector
+		else:
+			Wi_plus_05_n_plus_1_vector = Wi_plus_1_n_plus_1_vector
+			#ei_plus_1_n_plus_1_vector = ei_plus_1_n_plus_1_vector
 
+		yacheyka.Wi_plus_05_n_vector = Wi_plus_05_n_plus_1_vector
 
+		rhoi_n_plus_1 = yacheyka.rhoi_n + (yacheyka.dMi_minus_05_n - yacheyka.dMi_plus_05_n)/(A*dx)
+		Wi_n_plus_1 = yacheyka.rhoi_n * yacheyka.Wi_n / rhoi_n_plus_1 + (abs(yacheyka.dMi_minus_05_n)*yacheyka.Wi_minus_05_n_vector - abs(yacheyka.dMi_plus_05_n) * yacheyka.Wi_plus_05_n_vector)/(rhoi_n_plus_1*A*dx)
+		#ei_n_plus_1 = yacheyka.rhoi_n * yacheyka.ei_n / rhoi_n_plus_1 + (yacheyka.dMi_minus_05_n * ei_minus_05_n_plus_1 - dMi_plus_05_n * ei_plus_05_n_plus_1)/(rhoi_n_plus_1 * A * dx)
+		
+		yacheyka.change_density(rhoi_n_plus_1)
+		yacheyka.Wi_n = Wi_n_plus_1
+		
+		yacheyka.Pi_n = yacheyka.rhoi_n*Rg*yacheyka.Ti_n
+		rhoi
 
+		
 
 	t+=dt
+
+#for i in range(cell_count):
+	#print(pole[i].Pi_n)
+	#print(pole[i].Pi_plus_05_n)
+	#print(pole[i].Pi_minus_05_n)
+	#print(pole[i].Wi_n_vector)
+	#print(pole[i].Wi_n)
+	#print(pole[i].dMi_plus_05_n)
+	#print(pole[i].dMi_minus_05_n)
+print(M1, M2)
--- Kursovoy_bak_1/method_k_ya.py
+++ Kursovoy_bak_1/method_k_ya.py
@@ -2,31 +2,53 @@
 
 
 class Yacheyka:
-	def __init__(self, number, speed, pressure, rho):
+	def __init__(self, number, speed, pressure, rho, temp):
 		self.number = number
-		self.W = speed
-		self.P = pressure
-		self.rho = rho
-		self.P_i_plus_05 = pressure
-		self.P_i_minus_05 = pressure
-		self.W_i_plus_05 = speed
-		self.W_i_minus_05 = speed
-		self.e = 0
-		self.dMi_plus_05 = 0
-		self.dMi_minus_05 = 0
+		
+		self.Wi_n = speed
+		self.Wi_plus_05_n = speed
+		self.Wi_minus_05_n = speed
+
+		self.Wi_n_vector = speed
+		self.Wi_plus_05_n_vector = speed
+		self.Wi_minus_05_n_vector = speed
+
+		self.Pi_n = pressure
+		self.Pi_plus_05_n = pressure
+		self.Pi_minus_05_n = pressure
+
+		self.ei_n = 0
+		self.dMi_plus_05_n = 0
+		self.dMi_minus_05_n = 0
+		self.rhoi_n = rho
+		self.Ti_n = temp
 
 	def change_speed(self, new_speed):
-		self.W = new_speed
+		self.Wi_n_vector = new_speed
+
+	def change_Wi_plus_05_n(self, new_Wi_plus_05_n):
+		self.Wi_plus_05_n_vector = new_Wi_plus_05_n
+
+	def change_Wi_minus_05_n(self, new_Wi_minus_05_n):
+		self.Wi_minus_05_n_vector = new_Wi_minus_05_n
 
 	def change_pressure(self, new_pressure):
-		self.P = new_pressure
+		self.Pi_n = new_pressure
+
+	def change_Pi_plus_05_n(self, new_Pi_plus_05_n):
+		self.Pi_plus_05_n = new_Pi_plus_05_n
+
+	def change_Pi_minus_05_n(self, new_Pi_minus_05_n):
+		self.Pi_minus_05_n = new_Pi_minus_05_n
 
 	def change_energy(self, new_energy):
-		self.e = new_energy
+		self.ei_n = new_energy
 
-	def change_dM_plus(self, new_dM):
-		self.dMi_plus_05 = new_dM
+	def change_dMi_plus_05_n(self, new_dM):
+		self.dMi_plus_05_n = new_dM
 
-	def change_dM_minus(self, new_dM):
-		self.dMi_minus_05 = new_dM
+	def change_dMi_minus_05_n(self, new_dM):
+		self.dMi_minus_05_n = new_dM
 
+	def change_density(self, new_density):
+		self.rhoi_n = new_density
