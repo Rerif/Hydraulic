bb2f7e409ebff40f722e689e636d630567626f5b
Rerif <zahar_9898@mail.ru>
2020-09-22 22:40:39 +0600

    исправлена f1, теперь рассчитывется как отношение разностей давлений

--- Kursovoy_mag_1/src.py
+++ Kursovoy_mag_1/src.py
@@ -8,20 +8,11 @@ def obem(pfi):
 	dV = A*dt*v
 	return dV
 
-def dM_const_l(pump):
-	# комп секция
-	l1 = l1_0 + (Sh/2)*((1-math.cos(pfi_i_kompressora)) + (lyambda/4) * (1-math.cos(2*pfi_i_kompressora)))
-	# насосная секция 
-	l2 = l2_0 + (Sh/2)*((1-math.cos(pfi_i_nasosa)) + (lyambda/4) * (1-math.cos(2*pfi_i_nasosa)))
-	# давление в щели
-	p3 = (((delta_1_in_3/l1)*p1) + ((delta_1_in_3/l2)*p2))/((delta_1_in_3/l1)+(delta_1_in_3/l2))
-	if pump:
-		return rho * ((math.pi * dc * delta_1_in_3)/(12*muw*omega))*((p2 - p3)/l2)*dpfi
+def dM_delta_const(isPump):
+	if isPump:
+		return p2 - p1
 	else:
-		return rho * ((math.pi * dc * delta_1_in_3)/(12*muw*omega))*((p1 - p3)/l1)*dpfi
-
-
-
+		return p1 - p2
 
 # зазор со стороны компрессорной полости
 delta_1 = 50*10**(-6)
@@ -56,7 +47,7 @@ l2 = Sh
 lp = Sh
 l1_0 = Sh
 l2_0 = Sh
-lyambda = Sh/(2*l1)
+lyambda = 0.25
 
 # площади полости
 A = math.pi * dc * dc / 4
@@ -87,11 +78,13 @@ davlenie_kompressora = []
 obem_nasosa = []
 obem_kompressora = []
 for i in range(number_of_iterations + 1):
+        # текущий угол поворота вала
 	pfi_i = i*dpfi
+	# угол поворота в радианах
 	pfi_i_kompressora = (pfi_i)*math.pi/180
 	pfi_i_nasosa = (pfi_i + 180)*math.pi/180
 	# нагнетание жидкости в насосе, всасывание газа в компрессоре
-	if pfi_i < 180:
+	if 0 <= pfi_i < 180:
 		pump = True
 		# процесс нагнетания жидкости
 		p2 = pn_nasosa + dpn_nasosa
@@ -108,9 +101,10 @@ for i in range(number_of_iterations + 1):
 		# масса жидкости из насоса в компрессор при ступенчатой щели
 		dM2 = rho * ((math.pi * dc * delta_2_in_3)/(12*muw*omega))*((p2 - p3)/l2)*dpfi
 		M2 += dM2
-		dM20 = dM_const_l(pump)
-		#print(M20)
+
+		dM20 = dM_delta_const(pump)
 		M20 += dM20
+
 	# всасывание жидкости в насосе, нагнетание или сжатие газа в компрессоре
 	else:
 		pump = False
@@ -121,7 +115,6 @@ for i in range(number_of_iterations + 1):
 		V1 = (Vh/2)*((1-math.cos(pfi_i_kompressora)) + (lyambda/4)*(1-math.cos(2*pfi_i_kompressora))) + Vm
 		p1 = pvs_kompressora * ((V0/V1)**k)
 		# если происходит нагнетание
-		#print(p1)
 		if p1 >= pn_kompressora:	
 			p1 = pn_kompressora + dpn_kompressora
 		davlenie_kompressora.append(p1)	
@@ -132,10 +125,9 @@ for i in range(number_of_iterations + 1):
 		p3 = (((delta_1_in_3/l1)*p1) + ((delta_2_in_3/l2)*p2))/((delta_1_in_3/l1)+(delta_2_in_3/l2))
 		# масса жидкости из компрессора в насос
 		dM1 = rho * ((math.pi * dc * delta_1_in_3)/(12*muw*omega))*((p1 - p3)/l1)*dpfi
-		M1 += dM2
-		dM10 = dM_const_l(pump)
+		M1 += dM1
+		dM10 = dM_delta_const(pump)
 		M10 += dM10
-
 	Vnasosa_i += obem(pfi_i_nasosa)
 	Vkompressora_i += obem(pfi_i_kompressora)
 	obem_nasosa.append(Vnasosa_i)
@@ -144,14 +136,16 @@ for i in range(number_of_iterations + 1):
 f1 = M20/M10
 f2 = M2/M1
 f3 = f2/f1
-print("M20 = ", M20, "M10 = ", M10)
+
+#print("M20 = ", M20, "M10 = ", M10)
 print("f1 = M20/M10 =", f1)
-#print(pfi_i, dpfi)
 print("M2 = ", str(M2)+",", "M1 = ", M1)
 print("f2 = M2/M1 = ", f2)
 
-print("f3 = f2/f1", f3)
-'''
+print("f3 = f2/f1 = ", f3)
+
+
+
 davlenie_nasosa.append(pn_nasosa)
 davlenie_kompressora.append(pvs_kompressora)
 obem_nasosa.append(obem_nasosa[-1])
@@ -167,13 +161,13 @@ plt.subplot(2, 1, 1)
 plt.plot(x1, y1)               # построение графика
 plt.title("Зависимости давления от объема") # заголовок
 plt.xlabel("V камеры насоса")
-plt.ylabel("давление P1", fontsize=14) # ось ординат
+plt.ylabel("давление P2(насоса)", fontsize=14) # ось ординат
 plt.grid(True)                # включение отображение сетки
 plt.subplot(2, 1, 2)
 plt.plot(x2, y2)               # построение графика
 plt.xlabel("V камеры компрессора", fontsize=14)  # ось абсцисс
-plt.ylabel("Давление P2", fontsize=14) # ось ординат
+plt.ylabel("Давление P1(компрессора)", fontsize=14) # ось ординат
 plt.grid(True)
-plt.show()'''
+plt.show()
 
 
