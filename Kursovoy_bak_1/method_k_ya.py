9617047bfc24f0d68d731c0514ad1c748c73b782
Rerif <zahar_9898@mail.ru>
2020-11-02 18:11:22 +0600

    1.0 version kurs_mag_1

--- Kursovoy_bak_1/method_k_ya.py
+++ Kursovoy_bak_1/method_k_ya.py
@@ -9,46 +9,23 @@ class Yacheyka:
 		self.Wi_plus_05_n = speed
 		self.Wi_minus_05_n = speed
 
-		self.Wi_n_vector = speed
-		self.Wi_plus_05_n_vector = speed
-		self.Wi_minus_05_n_vector = speed
+		self.Wi_n_vec = speed
+		self.Wi_plus_05_n_vec = speed
+		self.Wi_minus_05_n_vec = speed
 
 		self.Pi_n = pressure
 		self.Pi_plus_05_n = pressure
 		self.Pi_minus_05_n = pressure
 
-		self.ei_n = 0
+		self.Mi_n = 1
+
+		self.Ei_n = 0
+		self.Ei_n_vec = 0
 		self.dMi_plus_05_n = 0
 		self.dMi_minus_05_n = 0
 		self.rhoi_n = rho
 		self.Ti_n = temp
 
-	def change_speed(self, new_speed):
-		self.Wi_n_vector = new_speed
+	
 
-	def change_Wi_plus_05_n(self, new_Wi_plus_05_n):
-		self.Wi_plus_05_n_vector = new_Wi_plus_05_n
 
-	def change_Wi_minus_05_n(self, new_Wi_minus_05_n):
-		self.Wi_minus_05_n_vector = new_Wi_minus_05_n
-
-	def change_pressure(self, new_pressure):
-		self.Pi_n = new_pressure
-
-	def change_Pi_plus_05_n(self, new_Pi_plus_05_n):
-		self.Pi_plus_05_n = new_Pi_plus_05_n
-
-	def change_Pi_minus_05_n(self, new_Pi_minus_05_n):
-		self.Pi_minus_05_n = new_Pi_minus_05_n
-
-	def change_energy(self, new_energy):
-		self.ei_n = new_energy
-
-	def change_dMi_plus_05_n(self, new_dM):
-		self.dMi_plus_05_n = new_dM
-
-	def change_dMi_minus_05_n(self, new_dM):
-		self.dMi_minus_05_n = new_dM
-
-	def change_density(self, new_density):
-		self.rhoi_n = new_density
